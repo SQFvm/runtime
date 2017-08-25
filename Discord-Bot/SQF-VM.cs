aa8bcf7d081d52d514e4da921edd0ddfabbe6235
X39 <X39@users.noreply.github.com>
2017-08-25 02:41:16 +0200

    removed extension from dllimport

--- Discord-Bot/SQF-VM.cs
+++ Discord-Bot/SQF-VM.cs
@@ -10,7 +10,7 @@ namespace Discord_Bot
 {
     public static class SQF_VM
     {
-        [DllImport(@"SQF-VM.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        [DllImport(@"SQF-VM", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
         public static extern unsafe IntPtr start_program(string input);
         public static unsafe string StartProgram(string content)
         {
