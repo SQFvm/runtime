8bf2d698ab091ff93c6c394549bb543aa8d7eb94
X39 <X39@users.noreply.github.com>
2017-10-02 22:50:55 +0200

    fixed macro

--- SQF-VM/random.h
+++ SQF-VM/random.h
@@ -1,6 +1,6 @@
 #ifndef _RANDOM_H_
 #define _RANDOM_H_
 
-#define randlf(LF) ((double)rand() / (double)(RAND_MAX / (LF)));
+#define randlf(LF) ((double)rand() / (double)(RAND_MAX / ((LF) + 1)))
 
 #endif // !_RANDOM_H_
