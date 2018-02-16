4e43fbfce0bf1f900fd6a101852418396ff52431
X39 <X39@users.noreply.github.com>
2018-02-17 00:13:42 +0100

    added typeOf OBJECT

--- sqfvm-cpp/objectcmds.cpp
+++ sqfvm-cpp/objectcmds.cpp
@@ -3,6 +3,7 @@
 #include "cmd.h"
 #include "virtualmachine.h"
 #include "objectdata.h"
+#include "innerobj.h"
 #include <algorithm>
 
 using namespace sqf;
@@ -12,8 +13,21 @@ namespace
 	{
 		return std::make_shared<value>(std::make_shared<objectdata>(), sqf::type::OBJECT);
 	}
+	std::shared_ptr<value> typeof_object(virtualmachine* vm, std::shared_ptr<value> right)
+	{
+		auto obj = right->data<objectdata>();
+		if (obj->isnull())
+		{
+			return std::make_shared<value>(L"OBJECT");
+		}
+		else
+		{
+			return std::make_shared<value>(obj->obj()->classname());
+		}
+	}
 }
 void sqf::commandmap::initobjectcmds(void)
 {
 	add(nular(L"objNull", L"A non-existent Object. To compare non-existent objects use isNull or isEqualTo.", objnull_));
+	add(unary(L"typeOf", sqf::type::OBJECT, L"Returns the config class name of given object.", typeof_object));
 }
