0f204c8cd7db34acab0d75ea386c1287826f4903
X39 <X39@users.noreply.github.com>
2018-02-17 01:19:10 +0100

    added while do

--- sqfvm-cpp/namespacecmds.cpp
+++ sqfvm-cpp/namespacecmds.cpp
@@ -4,6 +4,8 @@
 #include "virtualmachine.h"
 #include "sqfnamespace.h"
 #include "namespaces.h"
+#include "codedata.h"
+#include "callstack.h"
 #include <algorithm>
 
 std::shared_ptr<sqf::sqfnamespace> sqf::commands::namespaces::MissionNamespace = std::make_shared<sqf::sqfnamespace>(L"missionNamespace");
@@ -21,6 +23,19 @@ namespace
 		transform(r->varmap().begin(), r->varmap().end(), arr.begin(), [](auto pair) { return std::make_shared<value>(pair.first); });
 		return std::make_shared<value>(arr);
 	}
+	std::shared_ptr<value> with_namespace(virtualmachine* vm, std::shared_ptr<value> right)
+	{
+		auto r = right->data<sqfnamespace>();
+		return std::make_shared<value>(r, type::WITH);
+	}
+	std::shared_ptr<value> do_with_code(virtualmachine* vm, std::shared_ptr<value> left, std::shared_ptr<value> right)
+	{
+		auto l = std::static_pointer_cast<sqfnamespace>(left->data());
+		auto r = std::static_pointer_cast<codedata>(right->data());
+		auto cs = std::make_shared<callstack>(l);
+		r->loadinto(vm->stack());
+		return std::shared_ptr<value>();
+	}
 }
 void sqf::commandmap::initnamespacecmds(void)
 {
@@ -33,4 +48,6 @@ void sqf::commandmap::initnamespacecmds(void)
 	add(nular(L"profileNamespace", L"Returns the global namespace attached to the active user profile. Use setVariable and getVariable to save and load data to and from this Namespace. A variable can be deleted by setting its value to nil. By default the variables set in this namespace will exist while the game is running. In order to make variables save permanently, use saveProfileNamespace before the game is over.",
 		[](virtualmachine* vm) -> std::shared_ptr<value> { return std::make_shared<value>(sqf::commands::namespaces::profileNamespace(), sqf::type::NAMESPACE); }));
 	add(unary(L"allVariables", type::NAMESPACE, L"Returns a list of all variables from desired namespace.", allvariables_namespace));
+	add(unary(L"with", type::NAMESPACE, L"Creates a WITH type that is used inside a do construct in order to execute code inside a given namespace.", with_namespace));
+	add(binary(4, L"do", type::WITH, type::CODE, L"Executes code in the namespace provided via the WITH parameter.", do_with_code));
 }
