730e798c06b5d6908656760a5e01229ed2081e1a
X39 <X39@users.noreply.github.com>
2018-02-28 13:00:05 +0100

    moved C-Version into a 'history' branch (RIP)

--- SQF-VM/tokenizer.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TOKENIZER_H_
-#define _TOKENIZER_H_
-
-#ifndef _TEXTRANGE_H_
-#error "tokenizer.h" has to be included after "textrange.h" 
-#endif // !_SQF_TYPES_H_
-
-void tokenize(TR_ARR* arr, const wchar_t* code);
-#endif
