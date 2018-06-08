41c55527bb00f08e4bc9bb17ff002f3a27b8ce15
X39 <X39@users.noreply.github.com>
2018-06-08 17:34:12 +0200

    ?! Added networking server and json ... also added tclap for command line parsing

--- sqfvm-cpp/Entry.cpp
+++ sqfvm-cpp/Entry.cpp
@@ -1,3 +1,4 @@
+#include "netserver.h"
 #include "virtualmachine.h"
 #include "commandmap.h"
 #include "value.h"
@@ -7,8 +8,10 @@
 #include <sstream>
 #include <fstream>
 #include <cstdlib>
+#include <tclap\CmdLine.h>
 
 #include "python_externc.h"
+
 
 static std::string load_file(const std::string& filename);
 static std::vector<char> readFileW(const std::string& filename);
@@ -16,18 +19,8 @@ static std::vector<char> readFile(const std::string& filename);
 
 static std::string load_file(const std::string& filename)
 {
-	auto vec = readFileW(filename);
-	return std::string(vec.begin(), vec.end());
-}
-static std::vector<char> readFileW(const std::string& filename)
-{
 	auto vec = readFile(filename);
-	auto vecW = std::vector<char>(vec.size());
-	for (size_t i = 0; i < vec.size(); i++)
-	{
-		vecW[i] = vec[i];
-	}
-	return vecW;
+	return std::string(vec.begin(), vec.end());
 }
 static std::vector<char> readFile(const std::string& filename)
 {
@@ -35,7 +28,9 @@ static std::vector<char> readFile(const std::string& filename)
 
 	if (!file.is_open())
 	{
-		return std::vector<char>();
+		std::stringstream sstream("Could not open file '");
+		sstream << filename << "'.";
+		throw std::runtime_error(sstream.str());
 	}
 
 	size_t fileSize = (size_t)file.tellg();
@@ -49,82 +44,126 @@ static std::vector<char> readFile(const std::string& filename)
 	return buffer;
 }
 
-int main(int argc, char** argv)
+void StartServer()
 {
-	//py_init(100000);
-	//char buff[2000];
-	//py_exec("diag_log 12", buff, 2000);
-	//py_uninit();
 
-	auto vm = sqf::virtualmachine();
-	//std::string line;
-	//std::wcin >> line;
-	int i = 0;
-	printf("Please enter your SQF code.\nTo get the capabilities, use the `help__` instruction.\nTo run the code, Press <ENTER> twice.\n");
-	std::string line;
-	std::stringstream sstream;
-	do
-	{
-		printf("%d:\t", i++);
-		std::getline(std::cin, line);
-		sstream << line << std::endl;
-	} while (line.size() != 0);
-
-	std::wcout << std::endl;
-
-	/*
-	sqf::commandmap::get().init();
-
-	vm.out() << std::endl << "ASSEMBLY" << std::endl;
-	//vm.parse_assembly("push SCALAR 1; push SCALAR 1; callBinary +; push SCALAR 2; callBinary -;");
-	//vm.parse_assembly("push SCALAR 1; push STRING 'test'; push STRING 'test''escape'; push SCALAR 1; push SCALAR 2; push SCALAR 3; callnular ntest; makeArray 4; makeArray 4;");
-	//vm.parse_assembly("push SCALAR 12; assignToLocal _test;");
-	vm.execute();
-	
-	vm.out() << std::endl << "SQF" << std::endl;
-	//vm.parse_sqf("1 + 1 - 2; 0; {1 + 2 * 3 - 1 * 4}; 6; utest 1; ntest; [1, 'test', 'test''escape', [1, 2, 3, ntest]]; private _test = 12; _test2 = 13; _test; globalvar;");
-	//vm.parse_sqf("1 + 1 - 2;");
-	//vm.parse_sqf("[1, 'test', 'test''escape', [1, 2, 3, ntest]]");
-	//vm.parse_sqf("private _test = 12;");
-	vm.execute();
-
-	sqf::commandmap::get().uninit();
-	*/
-
-	sqf::commandmap::get().init();
-	//auto cd = std::make_shared<sqf::configdata>();
-
-	vm.parse_config(load_file("C:\\Users\\marco.silipo\\Downloads\\AiO.1.76.142872\\AiO.1.76.142872.cpp"), sqf::configdata::configFile()->data<sqf::configdata>());
-	vm.parse_sqf(sstream.str());
-	vm.execute();
-	std::shared_ptr<sqf::value> val;
-	bool success;
-	do {
-		val = vm.stack()->popval(success);
-		if (success)
-		{
-			vm.out() << "[WORK]\t<" << sqf::type_str(val->dtype()) << ">\t" << val->as_string() << std::endl;
-		}
-	} while (success);
-	sqf::commandmap::get().uninit();
-	std::wcout << std::endl;
-	system("pause");
 }
 
-/*
-    0       4         0       4    0
-[1, 2, 3] select ([0, 1, 2] select 2);
+int main(int argc, char** argv)
+{
+	TCLAP::CmdLine cmd("Emulates the ArmA-Series SQF environment.", ' ', "1.0");
+	TCLAP::MultiArg<std::string> loadSqfFileArg("f", "sqf-file", "Loads provided sqf-file from the hdd into the sqf-vm.", false, "PATH");
+	cmd.add(loadSqfFileArg);
 
-                  [0, 1, 2]        2
-[1, 2, 3]        (          select  )
-          select                     ;
-                       [0, 1]
-                 count          1
-[1, 2, 3]                     +
-          select                     ;
+	TCLAP::MultiArg<std::string> loadConfigFileArg("F", "config-file", "Loads provided config-file from the hdd into the sqf-vm.", false, "PATH");
+	cmd.add(loadConfigFileArg);
 
-- (...) wie konstanten wert behandeln
-- (...) rekursiv abarbeiten
-- array mit index führen
+	TCLAP::SwitchArg noPromptArg("a", "no-prompt", "Disables the prompt which expects you to type in sqf-code.", false);
+	cmd.add(noPromptArg);
 
-*/
+	TCLAP::SwitchArg useDebuggingServer("s", "start-server", "Causes the sqf-vm to start a network server allowing for automated control. Implicitly will cause --no-prompt to be set.", false);
+	cmd.add(useDebuggingServer);
+
+	TCLAP::ValueArg<int> maxInstructionsArg("m", "max-instructions", "Sets the maximum ammount of instructions to execute before a hard exit may occur. Setting this to 0 will disable the limit.", false, 0, "NUMBER");
+	cmd.add(maxInstructionsArg);
+
+	TCLAP::SwitchArg noPrintArg("n", "no-print", "Prevents the value stack to be printed out at the very end.", false);
+	cmd.add(noPrintArg);
+
+	cmd.parse(argc, argv);
+
+	std::vector<std::string> sqfFiles = loadSqfFileArg.getValue();
+	std::vector<std::string> configFiles = loadConfigFileArg.getValue();
+	bool noPrompt = noPromptArg.getValue() || useDebuggingServer.getValue();
+	bool startServer = useDebuggingServer.getValue();
+	int maxinstructions = maxInstructionsArg.getValue();
+	bool noPrint = noPrintArg.getValue();
+
+	auto vm = sqf::virtualmachine();
+	sqf::commandmap::get().init();
+	bool errflag = false;
+
+	//Load all sqf-files provided via arg.
+	for (auto& f : sqfFiles)
+	{
+		try
+		{
+			auto str = load_file(f);
+			vm.parse_sqf(str);
+		}
+		catch (std::runtime_error ex)
+		{
+			errflag = true;
+			std::cout << ex.what() << std::endl;
+		}
+	}
+
+	//Load & merge all config-files provided via arg.
+	for (auto& f : configFiles)
+	{
+		try
+		{
+			auto str = load_file(f);
+			vm.parse_config(str, sqf::configdata::configFile()->data<sqf::configdata>());
+		}
+		catch (std::runtime_error ex)
+		{
+			errflag = true;
+			std::cout << ex.what() << std::endl;
+		}
+	}
+
+	if (!noPrompt)
+	{
+		//Prompt user to type in code.
+		int i = 0;
+		printf("Please enter your SQF code.\nTo get the capabilities, use the `help__` instruction.\nTo run the code, Press <ENTER> twice.\n");
+		std::string line;
+		std::stringstream sstream;
+		do
+		{
+			printf("%d:\t", i++);
+			std::getline(std::cin, line);
+			sstream << line << std::endl;
+		} while (line.size() != 0);
+
+		std::cout << std::endl;
+		vm.parse_sqf(sstream.str());
+	}
+
+	if (startServer)
+	{
+		networking_init();
+	}
+
+	vm.execute();
+
+	if (!noPrint)
+	{
+		std::shared_ptr<sqf::value> val;
+		bool success;
+		do {
+			val = vm.stack()->popval(success);
+			if (success)
+			{
+				vm.out() << "[WORK]\t<" << sqf::type_str(val->dtype()) << ">\t" << val->as_string() << std::endl;
+			}
+		} while (success);
+		sqf::commandmap::get().uninit();
+		std::wcout << std::endl;
+	}
+
+	if (startServer)
+	{
+		networking_cleanup();
+	}
+
+	sqf::commandmap::get().uninit();
+
+	if (!noPrompt)
+	{
+		std::cout << std::endl << std::endl << "Press [ENTER] to continue.";
+		std::string line;
+		std::getline(std::cin, line);
+	}
+}
