2b1a3e5d1e786f797c5e7568973b20b8db52a265
X39 <X39@users.noreply.github.com>
2018-07-03 15:00:54 +0200

    enhanced the way the test-application works

--- sqfvm-debugger-test/Program.cs
+++ sqfvm-debugger-test/Program.cs
@@ -4,49 +4,100 @@ using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace sqfvm_debugger_test
 {
     class Program
     {
+        static Stream stream;
         static void Main(string[] args)
         {
-            try
-            {
-                using (var client = new TcpClient("localhost", 9090))
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Use CTRL-C to enter commands or to communicate with the server.");
+            Console.WriteLine("':?' will output help.");
+            Console.ResetColor();
+            bool continueExecution = true;
+            bool isInInterrupt = false;
+            Console.CancelKeyPress += (sender, e) => {
+                e.Cancel = true;
+                isInInterrupt = true;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("> ");
+                var input = Console.ReadLine();
+                Console.ResetColor();
+                if (String.IsNullOrWhiteSpace(input))
                 {
-                    var stream = client.GetStream();
-                    var buffer = new byte[1 << 11];
-                    while (client.Connected)
+                    return;
+                }
+                if (input[0] == ':')
+                {
+                    switch (input.Substring(1).ToLower())
                     {
-                        var read = stream.Read(buffer, 0, buffer.Length);
-                        if (read == 0)
-                        {
+                        case "q":
+                            continueExecution = false;
+                            e.Cancel = false;
                             break;
-                        }
+                        case "?":
+                        case "h":
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine(":? - displays this information.");
+                            Console.WriteLine(":h - displays this information.");
+                            Console.WriteLine(":q - exits the execution loop.");
+                            Console.ResetColor();
+                            break;
+                    }
+                    isInInterrupt = false;
+                    return;
+                }
+                if (stream == null)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Stream not yet connected. To quit, use ':q'");
+                    Console.ResetColor();
+                }
+                var bytes = Encoding.ASCII.GetBytes(input);
+                stream.Write(bytes, 0, bytes.Length);
+                stream.WriteByte(0);
+                isInInterrupt = false;
+            };
 
-                        var str = Encoding.ASCII.GetString(buffer, 0, read);
-                        Console.WriteLine(str);
-                        Console.Write("> ");
-                        var input = Console.ReadLine();
-                        if (String.IsNullOrWhiteSpace(input))
+            while (continueExecution)
+            {
+                try
+                {
+                    ConsoleCancelEventHandler handler = (sender, e) => { continueExecution = false; };
+                    using (var client = new TcpClient("localhost", 9090))
+                    {
+                        stream = client.GetStream();
+                        var buffer = new byte[1 << 11];
+                        while (client.Connected)
                         {
-                            continue;
+                            if (!continueExecution)
+                            {
+                                break;
+                            }
+
+                            var read = stream.Read(buffer, 0, buffer.Length);
+                            if (read == 0)
+                            {
+                                break;
+                            }
+                            var str = Encoding.ASCII.GetString(buffer, 0, read);
+                            while (isInInterrupt) { Thread.Sleep(100); }
+                            Console.WriteLine(str);
                         }
-                        var bytes = Encoding.ASCII.GetBytes(input);
-                        stream.Write(bytes, 0, bytes.Length);
-                        stream.WriteByte(0);
                     }
                 }
-            }
-            catch(Exception ex)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(ex.Message);
-                Console.ResetColor();
-                Console.WriteLine("Press any key to continue...");
-                Console.ReadKey();
+                catch (Exception ex)
+                {
+                    while (isInInterrupt) { Thread.Sleep(100); }
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.ResetColor();
+                }
             }
         }
     }
