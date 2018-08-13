45cd9670d25b81036b335f31168ac4f674b5acbb
X39 <X39@users.noreply.github.com>
2018-08-13 18:11:51 +0200

    fixed `>>` not getting recognized

--- sqfvm-cpp/parsesqf.cpp
+++ sqfvm-cpp/parsesqf.cpp
@@ -55,7 +55,7 @@ namespace sqf
 			//operator_ = [+-*/%^]|&&|\|\||==|[!<>][=]?|[a-zA-Z_]+;
 			size_t operator_(const char* code, size_t off) {
 				if (code[off] == '+' || code[off] == '-' || code[off] == '*' || code[off] == '/' || code[off] == '%' || code[off] == '^') return 1;
-				if ((code[off] == '|' && code[off + 1] == '|') || (code[off] == '&' && code[off + 1] == '&') || (code[off] == '=' && code[off + 1] == '=')) return 2;
+				if ((code[off] == '|' && code[off + 1] == '|') || (code[off] == '&' && code[off + 1] == '&') || (code[off] == '=' && code[off + 1] == '=') || (code[off] == '>' && code[off + 1] == '>')) return 2;
 				if (code[off] == '<' || code[off] == '>' || code[off] == '!')
 				{
 					if (code[off + 1] == '=') return 2;
