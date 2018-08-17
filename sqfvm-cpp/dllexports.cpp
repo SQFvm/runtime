9195c684a6b16acaa8fcbde941c61bc6ee74d461
X39 <X39@users.noreply.github.com>
2018-08-17 22:57:29 +0200

    now adding explicit \0 at the end of the buffer after executing

--- sqfvm-cpp/dllexports.cpp
+++ sqfvm-cpp/dllexports.cpp
@@ -34,6 +34,7 @@ extern "C" {
 		auto str = sstream.str();
 
 		std::strncpy(buffer, str.c_str(), bufferlen);
+		buffer[str.length() >= bufferlen ? bufferlen - 1 : str.length()] = '\0';
 	}
 
 	DLLEXPORT_PREFIX void sqfvm_loadconfig(const char* cfg)
