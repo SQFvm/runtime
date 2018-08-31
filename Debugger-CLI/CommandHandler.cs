f9fe3660541583568e73d8dddaea0a15758a80b1
X39 <X39@users.noreply.github.com>
2018-08-31 03:37:20 +0200

    Made debuger-test project a fully usable CLI implementation of the debugger

---
+++ Debugger-CLI/CommandHandler.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DebuggerCLI
+{
+    public class CommandHandler
+    {
+        public IEnumerable<ICommandHandlerItem> Commands => this._Commands;
+        private readonly List<ICommandHandlerItem> _Commands;
+        public CommandHandler()
+        {
+            this._Commands = new List<ICommandHandlerItem>()
+            {
+                new CommandHandlerItem("?", "Displays this information.", this.HelpMethod),
+                new CommandHandlerItem<string>("help", "Displays the detailed description for a command if available.", this.HelpDetailsMethod)
+            };
+        }
+        private void HelpDetailsMethod(string name)
+        {
+            ICommandHandlerItem cmd = this._Commands.FirstOrDefault((it) => it.Name.Equals(name) || it.ShortName.Equals(name));
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            if (cmd == null)
+            {
+                Console.WriteLine($"Could not find a command with the name '{name}'.");
+            }
+            else
+            {
+                name = GetHelpName(cmd);
+                var maxname = name.Length;
+                var output = $":{name}{new string(' ', maxname - name.Length)} - ";
+                Console.Write(output);
+                PrintAccording(output.Length, String.Join("\n", cmd.Description, cmd.Details), true);
+            }
+            Console.ResetColor();
+        }
+        private void HelpMethod()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            var maxname = this._Commands.Max((it) => GetHelpName(it).Length);
+            foreach (var cmd in this._Commands)
+            {
+                var name = GetHelpName(cmd);
+                var output = $":{name}{new string(' ', maxname - name.Length)} - ";
+                Console.Write(output);
+                PrintAccording(output.Length, cmd.Description, true);
+            }
+            Console.ResetColor();
+        }
+
+        private void PrintAccording(int leftspace, string content, bool startprinted)
+        {
+            var space = Console.WindowWidth - leftspace - 1 <= 0 ? 1 : Console.WindowWidth - leftspace - 1;
+
+            var lines = content.Split('\n').Where((l) => !String.IsNullOrWhiteSpace(l));
+            foreach (var line in lines)
+            {
+                var index = 0;
+                while (index < line.Length)
+                {
+                    var substr = line.Substring(index, line.Length - index < space ? line.Length - index : space);
+                    if (substr.Length == space)
+                    {
+                        var index2 = substr.LastIndexOf(' ');
+                        if (index2 > 0)
+                        {
+                            substr = substr.Substring(0, index2);
+                        }
+                    }
+                    index += substr.Length;
+                    if (startprinted)
+                    {
+                        startprinted = false;
+                    }
+                    else
+                    {
+                        Console.Write(new string(' ', leftspace));
+                    }
+                    Console.WriteLine(substr.Trim());
+                }
+            }
+        }
+        private static string GetHelpName(ICommandHandlerItem cmd)
+        {
+            if (String.IsNullOrWhiteSpace(cmd.ShortName))
+            {
+                return cmd.Name;
+            }
+            else
+            {
+                var index = cmd.Name.IndexOf(cmd.ShortName);
+                if (index == -1)
+                {
+                    return $"{cmd.Name} ({cmd.ShortName})";
+                }
+                else
+                {
+                    return $"{cmd.Name.Substring(0, index)}[{cmd.ShortName}]{cmd.Name.Substring(index + cmd.ShortName.Length)}";
+                }
+            }
+        }
+
+        public void Add(ICommandHandlerItem cmd)
+        {
+            if (this._Commands.Any((it) => it.Name.Equals(cmd.Name)))
+            {
+                throw new InvalidOperationException("Command already existing.");
+            }
+            this._Commands.Add(cmd);
+        }
+
+        public bool TryHandle(string input)
+        {
+            if (input.Length > 1 && input[0] == ':')
+            {
+                input = input.Substring(1);
+                var index = input.IndexOf(' ');
+                var command = (index == -1 ? input : input.Substring(0, index)).ToLowerInvariant();
+                var content = index == -1 ? String.Empty : input.Substring(index + 1);
+                foreach (var cmd in this._Commands)
+                {
+                    if (cmd.Name.Equals(command) || cmd.ShortName.Equals(command))
+                    {
+                        var values = Regex.Matches(content, @"[\""].+?[\""]|[^ ]+").Cast<Match>().Select(m => m.Value.Trim()).ToArray();
+                        try
+                        {
+                            cmd.Execute(values);
+                        }
+                        catch(Exception ex)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Black;
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.Write("Execution failed:");
+                            Console.WriteLine(ex.Message);
+                            Console.WriteLine($"Use `:help {cmd.Name}` to get help on this command.");
+                            Console.ResetColor();
+                        }
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Debugger-CLI/Program.cs
@@ -0,0 +1,173 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DebuggerCLI
+{
+    class Program
+    {
+        static Stream stream;
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Use CTRL-C to enter commands or to communicate with the server.");
+            Console.WriteLine("Arguments are separated via space. Using quotes (\"), it is possible to chain arguments.");
+            Console.WriteLine("':?' will output help.");
+            Console.ResetColor();
+            var continueExecution = true;
+            var isInInterrupt = false;
+            var toExecute = String.Empty;
+            var handler = new CommandHandler();
+            handler.Add(new CommandHandlerItem("q", "quit", "Shuts down the application.", () => Environment.Exit(0)));
+            handler.Add(new CommandHandlerItem("cs", "callstack", "Sends a request to get the current callstack.", () =>
+            {
+                var obj = JsonConvert.DeserializeObject(@"{""mode"":""get-callstack"",""data"":null}");
+                toExecute = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            }));
+            handler.Add(new CommandHandlerItem<string, string>("gv", "getvariable", "Sends a request to get a variable from the connected VM.", (var, scope) =>
+            {
+                if (String.IsNullOrWhiteSpace(var)) { throw new ArgumentException("Missing argument 1"); }
+                if (String.IsNullOrWhiteSpace(scope)) { throw new ArgumentException("Missing argument 2"); }
+                var obj = JsonConvert.DeserializeObject($@"{{ ""mode"": ""get-variables"", ""data"": [ {{ ""name"": ""{var}"", ""scope"": ""{scope}"" }} ] }}");
+                toExecute = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            }).SetDetails(
+                "Expects 2 input arguments:",
+                "- variablename to get.",
+                "- scope to get from.",
+                String.Join(" ",
+                    "If the variable is private, the scope argument is expected to be a NUMBER in the range [0,-N) indicating where",
+                    "the search should start. 0 will be the top, -N being how many scopes one would want to 'ignore'."
+                ),
+                "If the variable is not private, the scope is expected to be one of the following:",
+                "- missionNamespace",
+                "- uiNamespace",
+                "- profileNamespace",
+                "- parsingNamespace"
+            ));
+            handler.Add(new CommandHandlerItem<string>("c", "control", "Changes the current state of the VM.", (status) =>
+            {
+                if (String.IsNullOrWhiteSpace(status)) { throw new ArgumentException("Missing argument 1"); }
+                var obj = JsonConvert.DeserializeObject($@"{{ ""mode"": ""control"", ""data"": {{ ""status"": ""{status}"" }} }}");
+                toExecute = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            }).SetDetails(
+                "Expects 1 input arguments:",
+                "- status.",
+                "Status has to be one of the following enum values:",
+                "- stop",
+                "- pause",
+                "- resume",
+                "- quit"
+            ));
+            handler.Add(new CommandHandlerItem<int, string>("bp", "breakpoint", "Adds a breakpoint at provided line and file.", (line, file) =>
+            {
+                var obj = JsonConvert.DeserializeObject($@"{{ ""mode"": ""set-breakpoint"", ""data"": {{ ""line"": {line}, ""file"": ""{(file == null ? "" : file)}"" }} }}");
+                toExecute = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            }).SetDetails(
+                "Expects up to 2 input arguments:",
+                "- line number to break on.",
+                "- OPTIONAL file name."
+            ));
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                e.Cancel = true;
+                isInInterrupt = true;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("> ");
+                var input = Console.ReadLine();
+                Console.ResetColor();
+                byte[] bytes;
+                if (String.IsNullOrWhiteSpace(input))
+                {
+                    return;
+                }
+
+                if (handler.TryHandle(input))
+                {
+                    if (!String.IsNullOrWhiteSpace(toExecute))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"[-->][{DateTime.Now}] SEND");
+                        Console.WriteLine(toExecute);
+                        Console.ResetColor();
+                        bytes = Encoding.ASCII.GetBytes(toExecute);
+                        stream.Write(bytes, 0, bytes.Length);
+                        stream.WriteByte(0);
+                        toExecute = String.Empty;
+                    }
+                    isInInterrupt = false;
+                    return;
+                }
+                if (stream == null)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Not yet connected.");
+                    Console.ResetColor();
+                    return;
+                }
+                bytes = Encoding.ASCII.GetBytes(input);
+                stream.Write(bytes, 0, bytes.Length);
+                stream.WriteByte(0);
+                isInInterrupt = false;
+            };
+
+            while (continueExecution)
+            {
+                try
+                {
+                    using (var client = new TcpClient("localhost", 9090))
+                    {
+                        stream = client.GetStream();
+                        var buffer = new byte[1 << 11];
+                        while (client.Connected)
+                        {
+                            if (!continueExecution)
+                            {
+                                break;
+                            }
+
+                            var read = stream.Read(buffer, 0, buffer.Length);
+                            if (read == 0)
+                            {
+                                break;
+                            }
+                            var recv = Encoding.ASCII.GetString(buffer, 0, read);
+                            foreach (var str in recv.Split('\0').Where((s) => !String.IsNullOrWhiteSpace(s)))
+                            {
+                                while (isInInterrupt) { Thread.Sleep(100); }
+                                try
+                                {
+                                    var obj = JsonConvert.DeserializeObject(str);
+                                    var output = JsonConvert.SerializeObject(obj, Formatting.Indented);
+                                    Console.WriteLine($"[<--][{DateTime.Now}] RECEIVE");
+                                    Console.WriteLine(output);
+                                }
+                                catch(Exception ex)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine($"{DateTime.Now} - {ex.Message}");
+                                    Console.ResetColor();
+                                    Console.WriteLine($"[<--][{DateTime.Now}] RECEIVE WITH JSON PARSE ERR");
+                                    Console.WriteLine(str);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    stream = null;
+                    while (isInInterrupt) { Thread.Sleep(100); }
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{DateTime.Now} - {ex.Message}");
+                    Console.ResetColor();
+                }
+            }
+        }
+    }
+}
