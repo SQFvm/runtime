dbb6af2bf93f8d51bedb7e9e84f27ae21dd908ce
X39 <X39@users.noreply.github.com>
2018-09-07 19:08:23 +0200

    scalardata now uses float (like actual arma does) instead of double precision

--- src/scalardata.h
+++ src/scalardata.h
@@ -8,23 +8,22 @@ namespace sqf
 	class scalardata : public data
 	{
 	private:
-		double mvalue;
+		float mvalue;
 	public:
 		scalardata(void) : mvalue(0) {}
-		scalardata(char num) : mvalue((double)num) {}
-		scalardata(short num) : mvalue((double)num) {}
-		scalardata(int num) : mvalue((double)num) {}
-		scalardata(long num) : mvalue((double)num) {}
-		scalardata(float num) : mvalue((double)num) {}
-		scalardata(double num) : mvalue((double)num) {}
-		scalardata(size_t num) : mvalue((double)num) {}
+		scalardata(char num) : mvalue((float)num) {}
+		scalardata(short num) : mvalue((float)num) {}
+		scalardata(int num) : mvalue((float)num) {}
+		scalardata(long num) : mvalue((float)num) {}
+		scalardata(float num) : mvalue((float)num) {}
+		scalardata(double num) : mvalue((float)num) {}
+		scalardata(size_t num) : mvalue((float)num) {}
 		virtual std::string tosqf(void) const { return std::to_string(mvalue); }
 		operator char(void) const { return (char)mvalue; }
 		operator short(void) const { return (short)mvalue; }
 		operator int(void) const { return (int)mvalue; }
 		operator long(void) const { return (long)mvalue; }
 		operator float(void) const { return (float)mvalue; }
-		operator double(void) const { return (double)mvalue; }
 		operator size_t(void) const { return (size_t)mvalue; }
 		virtual bool equals(std::shared_ptr<data> d) const { return mvalue == std::dynamic_pointer_cast<scalardata>(d)->mvalue; }
 	};
