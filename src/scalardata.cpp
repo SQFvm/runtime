a54c411e048647808e85de89799f36d6fd5506cf
X39 <X39@users.noreply.github.com>
2018-09-29 13:05:34 +0200

    fixed scalardata string containing a \0

--- src/scalardata.cpp
+++ src/scalardata.cpp
@@ -11,7 +11,7 @@ std::string sqf::scalardata::tosqf(void) const
 		auto bufflen = std::snprintf(0, 0, "%g", mvalue) + 1;
 		auto buff = new char[bufflen];
 		std::snprintf(buff, bufflen, "%g", mvalue);
-		auto str = std::string(buff, bufflen);
+		auto str = std::string(buff, bufflen - 1);
 		delete[] buff;
 		return str;
 	}
@@ -20,7 +20,7 @@ std::string sqf::scalardata::tosqf(void) const
 		auto bufflen = std::snprintf(0, 0, "%0.*f", decimals, mvalue) + 1;
 		auto buff = new char[bufflen];
 		std::snprintf(buff, bufflen, "%0.*f", decimals, mvalue);
-		auto str = std::string(buff, bufflen);
+		auto str = std::string(buff, bufflen - 1);
 		delete[] buff;
 		return str;
 	}
