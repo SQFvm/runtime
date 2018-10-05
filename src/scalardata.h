4e97f2f055ac8121abdaab0a6b217d8954496cc1
Dedmen Miller <dedmen@dedmen.de>
2018-10-05 11:50:51 +0200

    Cleanup scalardata constructors and operators
    that were not used and don't make sense

--- src/scalardata.h
+++ src/scalardata.h
@@ -12,20 +12,9 @@ namespace sqf
 		static int decimals;
 	public:
 		scalardata() : mvalue(0) {}
-		scalardata(char num) : mvalue(static_cast<float>(num)) {}
-		scalardata(short num) : mvalue(static_cast<float>(num)) {}
-		scalardata(int num) : mvalue(static_cast<float>(num)) {}
-		scalardata(long num) : mvalue(static_cast<float>(num)) {}
 		scalardata(float num) : mvalue(static_cast<float>(num)) {}
-		scalardata(double num) : mvalue(static_cast<float>(num)) {}
-		scalardata(size_t num) : mvalue(static_cast<float>(num)) {}
 		virtual std::string tosqf() const;
-		operator char() const { return static_cast<char>(mvalue); }
-		operator short() const { return static_cast<short>(mvalue); }
-		operator int() const { return static_cast<int>(mvalue); }
-		operator long() const { return static_cast<long>(mvalue); }
 		operator float() const { return static_cast<float>(mvalue); }
-		operator size_t() const { return static_cast<size_t>(mvalue); }
 		virtual bool equals(std::shared_ptr<data> d) const {
 			const auto scalData = std::dynamic_pointer_cast<scalardata>(d);
 			if (!scalData) return false;
