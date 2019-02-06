4350adc734b97a59e6f4ab9fc2be0722bdf3934d
X39 <X39@users.noreply.github.com>
2019-02-06 22:28:41 +0100

    Fixed invalid initialization order causing segfault

--- src/callstack_apply.h
+++ src/callstack_apply.h
@@ -10,12 +10,12 @@ namespace sqf
 	private:
 		size_t mcurindex;
 		bool mend;
-		std::vector<std::shared_ptr<value>> marr;
 		std::vector<std::shared_ptr<value>> moutarr;
+		std::vector<std::shared_ptr<value>> marr;
 		std::shared_ptr<codedata> mapply;
 	public:
 		callstack_apply(std::shared_ptr<sqf::sqfnamespace> ns, std::vector<std::shared_ptr<value>> arr, std::shared_ptr<codedata> apply)
-			: callstack(ns), mcurindex(0), mend(arr.empty()), marr(std::move(arr)), moutarr(arr.size()), mapply(std::move(apply)) {}
+			: callstack(ns), mcurindex(0), mend(arr.empty()), moutarr(arr.size()), marr(std::move(arr)), mapply(std::move(apply)) { }
 		std::shared_ptr<sqf::instruction> popinst(sqf::virtualmachine* vm) override;
 	};
 }
