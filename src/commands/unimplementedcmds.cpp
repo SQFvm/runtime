72ea23c3da0f0d69a154ea30ce1116f71a978f19
Dedmen Miller <dedmen@dedmen.de>
2019-03-11 12:00:03 +0100

    add getPylonMagazines unimplemented cmd

--- src/commands/unimplementedcmds.cpp
+++ src/commands/unimplementedcmds.cpp
@@ -1956,7 +1956,8 @@ void sqf::commandmap::initunimplemented()
 	add(unary("ctrlsetfocus", type::ANY, "", [](virtualmachine* vm, std::shared_ptr<value> r) -> std::shared_ptr<value> { vm->err() << "NOT IMPLEMENTED (ctrlsetfocus)." << std::endl; return std::make_shared<value>(); })); /* CONTROL */
 	add(unary("unitaddons", type::ANY, "", [](virtualmachine* vm, std::shared_ptr<value> r) -> std::shared_ptr<value> { vm->err() << "NOT IMPLEMENTED (unitaddons)." << std::endl; return std::make_shared<value>(); })); /* STRING */
 	add(unary("parsesimplearray", type::ANY, "", [](virtualmachine* vm, std::shared_ptr<value> r) -> std::shared_ptr<value> { vm->err() << "NOT IMPLEMENTED (parsesimplearray)." << std::endl; return std::make_shared<value>(); })); /* STRING */
-
+	add(unary("getpylonmagazines", type::ANY, "", [](virtualmachine* vm, std::shared_ptr<value> r) -> std::shared_ptr<value> { vm->err() << "NOT IMPLEMENTED (getpylonmagazines)." << std::endl; return std::make_shared<value>(); })); /* STRING */
+    
 
 
 	add(nular("safezonex", "", [](virtualmachine* vm) -> std::shared_ptr<value> { vm->err() << "NOT IMPLEMENTED (safezonex)." << std::endl; return std::make_shared<value>(); }));
