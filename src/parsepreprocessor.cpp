623145b8d20d65ba7a368639359ca13f4c315e3d
marco.silipo <m.silipo@web.de>
2019-04-27 13:27:22 +0200

    Fixed double quoted strings are not skipped

--- src/parsepreprocessor.cpp
+++ src/parsepreprocessor.cpp
@@ -195,33 +195,48 @@ namespace {
 	{
 		auto currentOffset = fileinfo.off;
 		bool flag = true;
+		bool in_string = false;
 		while (flag)
 		{
-			char c = fileinfo.peek();
-			switch (c)
+			if (in_string)
 			{
-			case 'a': case 'b': case 'c': case 'd': case 'e':
-			case 'f': case 'g': case 'h': case 'i': case 'j':
-			case 'k': case 'l': case 'm': case 'n': case 'o':
-			case 'p': case 'q': case 'r': case 's': case 't':
-			case 'u': case 'v': case 'w': case 'x': case 'y':
-			case 'z': case 'A': case 'B': case 'C': case 'D':
-			case 'E': case 'F': case 'G': case 'H': case 'I':
-			case 'J': case 'K': case 'L': case 'M': case 'N':
-			case 'O': case 'P': case 'Q': case 'R': case 'S':
-			case 'T': case 'U': case 'V': case 'W': case 'X':
-			case 'Y': case 'Z': case '0': case '1': case '2':
-			case '3': case '4': case '5': case '6': case '7':
-			case '8': case '9': case '_':
-			case '\n': case '\\':
-			case '#':
-			case '\0':
-				flag = false;
-				break;
-			case '\r':
-				break;
-			default:
-				sstream << fileinfo.next();
+				char c = fileinfo.next();
+				if (c == '"')
+				{
+					in_string = false;
+				}
+				sstream << c;
+			}
+			else
+			{
+				char c = fileinfo.peek();
+				switch (c)
+				{
+				case 'a': case 'b': case 'c': case 'd': case 'e':
+				case 'f': case 'g': case 'h': case 'i': case 'j':
+				case 'k': case 'l': case 'm': case 'n': case 'o':
+				case 'p': case 'q': case 'r': case 's': case 't':
+				case 'u': case 'v': case 'w': case 'x': case 'y':
+				case 'z': case 'A': case 'B': case 'C': case 'D':
+				case 'E': case 'F': case 'G': case 'H': case 'I':
+				case 'J': case 'K': case 'L': case 'M': case 'N':
+				case 'O': case 'P': case 'Q': case 'R': case 'S':
+				case 'T': case 'U': case 'V': case 'W': case 'X':
+				case 'Y': case 'Z': case '0': case '1': case '2':
+				case '3': case '4': case '5': case '6': case '7':
+				case '8': case '9': case '_':
+				case '\n': case '\\':
+				case '#':
+				case '\0':
+					flag = false;
+					break;
+				case '\r':
+					break;
+				case '"':
+					in_string = true;
+				default:
+					sstream << fileinfo.next();
+				}
 			}
 		}
 	}
@@ -257,12 +272,12 @@ namespace {
 
 		std::stringstream sstream;
 		
-
 		char c;
 		while (true)
 		{
 			replace_skip(local_fileinfo, sstream);
 			c = local_fileinfo.peek();
+
 			if (c == '#')
 			{
 				local_fileinfo.next();
