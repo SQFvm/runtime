7b3d445978fc79408a5ff9ba1adbf2d1192732bb
marco.silipo <m.silipo@web.de>
2020-02-02 10:54:55 +0100

    Moved files

---
+++ src/parsing/astnode.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+#include <vector>
+
+
+namespace sqf
+{
+	namespace parse
+	{
+		struct astnode
+		{
+			size_t offset;
+			size_t length;
+			size_t line;
+			size_t col;
+			std::string content;
+			std::string file;
+			short kind;
+			std::vector<astnode> children;
+		};
+
+		void print_navigate_ast(std::basic_ostream<char, std::char_traits<char>>* outstreamptr, const astnode& node, const char* (*astkindname)(short), unsigned short level = 0);
+	}
+}
