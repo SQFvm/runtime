78e9bcaeab5b6315c24b5e12a6cc0fb32b6c5d2c
X39 <X39@users.noreply.github.com>
2020-10-18 17:29:07 +0200

    Documentation

--- src/runtime/parser/config.h
+++ src/runtime/parser/config.h
@@ -4,29 +4,38 @@
 
 namespace sqf
 {
-	namespace runtime
-	{
-		class runtime;
-		class confighost;
-		namespace parser
-		{
-			class config
-			{
-			public:
-				virtual ~config() {}
-				virtual bool check_syntax(std::string contents, ::sqf::runtime::fileio::pathinfo pathinfo) = 0;
-				virtual bool parse(::sqf::runtime::confighost& target, std::string contents, ::sqf::runtime::fileio::pathinfo pathinfo) = 0;
-			};
-		}
-	}
-	namespace parser::config
-	{
-		class disabled : public ::sqf::runtime::parser::config
-		{
-		public:
-			virtual ~disabled() override { return; }
-			virtual bool check_syntax(std::string contents, ::sqf::runtime::fileio::pathinfo pathinfo) override { return false; }
-			virtual bool parse(::sqf::runtime::confighost& target, std::string contents, ::sqf::runtime::fileio::pathinfo pathinfo) override { return false; }
-		};
-	}
+    namespace runtime
+    {
+        class runtime;
+        class confighost;
+        namespace parser
+        {
+            class config
+            {
+            public:
+                virtual ~config() {}
+                /// Checks the syntax of the given contents.
+                /// @param  contents    The actual file contents.
+                /// @param  pathinfo    The path to the file.
+                /// @returns            True on success. False on Failure.
+                virtual bool check_syntax(std::string contents, ::sqf::runtime::fileio::pathinfo pathinfo) = 0;
+                /// Parses the given contents.
+                /// @param  target      The targeted confighost, the config should be parsed into.
+                /// @param  contents    The actual file contents.
+                /// @param  pathinfo    The path to the file.
+                /// @returns            True on success. False on Failure.
+                virtual bool parse(::sqf::runtime::confighost& target, std::string contents, ::sqf::runtime::fileio::pathinfo pathinfo) = 0;
+            };
+        }
+    }
+    namespace parser::config
+    {
+        class disabled : public ::sqf::runtime::parser::config
+        {
+        public:
+            virtual ~disabled() override { return; }
+            virtual bool check_syntax(std::string contents, ::sqf::runtime::fileio::pathinfo pathinfo) override { return false; }
+            virtual bool parse(::sqf::runtime::confighost& target, std::string contents, ::sqf::runtime::fileio::pathinfo pathinfo) override { return false; }
+        };
+    }
 }
