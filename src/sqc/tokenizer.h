bb087f7915bf45fd8e2cb4384b1eb0cc5c2d4094
Björn Dahlgren <bjorn@dahlgren.at>
2021-01-21 11:47:52 +0100

    Fix sqf::sqc::tokenizer::m_column will be initialized after

--- src/sqc/tokenizer.h
+++ src/sqc/tokenizer.h
@@ -504,7 +504,7 @@ namespace sqf::sqc
         }
 
     public:
-        tokenizer(iterator start, iterator end, std::string path) : m_start(start), m_current(start), m_end(end), m_line(0), m_column(0), m_mode(emode::normal) {}
+        tokenizer(iterator start, iterator end, std::string path) : m_start(start), m_current(start), m_end(end), m_mode(emode::normal), m_line(0), m_column(0) {}
         token next()
         {
             if (m_current == m_end) { return { etoken::eof, m_line, m_column, (size_t)(m_current - m_start), {} }; };
